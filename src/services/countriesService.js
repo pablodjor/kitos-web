219e01a132a83d560cfd6cc189146edabcecd522
Pablo <pablodjor@gmail.com>
2026-06-12 08:54:16 +0200

    fix countries

--- src/services/countriesService.js
+++ src/services/countriesService.js
@@ -1,30 +1,18 @@
-const REST_COUNTRIES_URL =
-  "https://restcountries.com/v3.1/all?fields=name,translations,cca2";
-
-let countriesCache = null;
-
 export async function getCountries() {
-  if (countriesCache) {
-    return countriesCache;
-  }
-
-  const response = await fetch(REST_COUNTRIES_URL);
+  const response = await fetch(
+    "https://countriesnow.space/api/v0.1/countries/positions"
+  );
 
   if (!response.ok) {
     throw new Error("No se pudieron cargar los países");
   }
 
-  const data = await response.json();
+  const result = await response.json();
 
-  countriesCache = data
+  return result.data
     .map((country) => ({
-      code: country.cca2,
-      name:
-        country.translations?.spa?.common ||
-        country.name?.common ||
-        country.cca2,
+      name: country.name,
+      code: country.iso2,
     }))
-    .sort((a, b) => a.name.localeCompare(b.name, "es"));
-
-  return countriesCache;
+    .sort((a, b) => a.name.localeCompare(b.name));
 }
